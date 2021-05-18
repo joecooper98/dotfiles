4490ff629918a65b2fe5ec6837896b1c8967a55e
SmokeyJoe <josephcharlescooper@gmail.com>
2021-05-18 16:18:38 +0100

    fixes

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 cp -rf .vim/ ~/.vim
 cp -f .vimrc ~/.vimrc
 mkdir ~/.vim/bundle
-git clone https://github.com/VundleVim/Vunlde.vim
+git clone https://github.com/VundleVim/Vundle.vim
 vim +PluginInstall +qall
 
